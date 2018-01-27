ae04546e4193cf0c8618b9e4e5ae431a804c38ae
Olga <nowacka.olga@wp.pl>
2018-01-27 21:55:28 +0100

    service added to have list on "about" page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -33,9 +34,11 @@ export class HomeComponent implements OnInit {
   goalText: string = "My first goal";
   goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
     this.itemsCount = this.goals.length;
   }
 
@@ -43,11 +46,13 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemsCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i){
     this.goals.splice(i, 1);
     this.itemsCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
 }
